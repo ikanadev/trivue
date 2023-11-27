801f2cf9f5572a8a9cd99bae2bac22bbc2f9028b
Kevin <vargaskevv@gmail.com>
2023-11-27 16:24:07 -0400

    New quiz form

--- src/views/NewQuiz.vue
+++ src/views/NewQuiz.vue
@@ -1,37 +1,94 @@
 <script setup lang="ts">
 import ThemeButton from '@/components/ThemeButton.vue';
 import IconChevBack from '@/components/icons/IconChevBack.vue';
-
+import IconCheck from '@/components/icons/IconCheck.vue';
+import IconClose from '@/components/icons/IconClose.vue';
+import IconPlus from '@/components/icons/IconPlus.vue';
 import { RouterLink } from 'vue-router';
+
 import { ref } from 'vue';
 
+type FormChoice = { text: string, correct: boolean };
+const defaultChoices = [{ text: 'From 2 to 4 choices', correct: false }, { text: 'One must be correct', correct: true }];
+
 const question = ref('');
+const choices = ref<FormChoice[]>(defaultChoices);
+const includeExplanation = ref(false);
+const explanation = ref('');
+
+function markAsCorrect(choice: FormChoice) {
+  const prev = choices.value.find(c => c.correct);
+  if (prev) prev.correct = false;
+  choice.correct = true;
+}
+
+function deleteChoice(index: number) {
+  const choice = choices.value[index];
+  if (choice.correct) {
+    choices.value[(index + 1) % choices.value.length].correct = true;
+  }
+  choices.value.splice(index, 1);
+}
+
 </script>
 <template>
   <main class="py2">
     <div class="flex justify-between items-center">
-      <RouterLink :to="{name: 'home'}" class="flex items-center underline underline-2 underline-offset-3 font-semibold">
-        <IconChevBack class="mt1 me1" />
+      <RouterLink :to="{ name: 'home' }" class="link flex items-center">
+        <IconChevBack class="me-1" />
         <span>Back</span>
       </RouterLink>
       <ThemeButton />
     </div>
     <h1 class="text-2xl text-main font-semibold text-center font-mono">NEW QUIZ</h1>
     <p class="text-center text-sm">Basic markdown supported</p>
-    <div class="flex flex-col">
-      <label for="question_body" class="mb0.5">Question:</label>
-      <textarea v-model="question" id="question_body"
-        class="font-mono bg-transparent py1 px2 b-main b-3 focus:outline-none focus:shadow-focus-main" rows="5">
-      </textarea>
-      <p class="mb0.5 mt2">Choices:</p>
-      <div class="flex items-center">
-        <div>V</div>
-        <textarea v-model="question" id="question_body"
-          class="font-mono bg-transparent py1 px2 b-secondary b-3 focus:outline-none focus:shadow-focus-main flex-1" rows="1">
+    <div class="flex flex-col gap-4">
+      <div class="form-control">
+        <label class="label" for="question_body">
+          <span class="label-text">Question:</span>
+        </label>
+        <textarea v-model="question" id="question_body" class="textarea textarea-bordered textarea-sm font-mono" rows="5">
         </textarea>
-        <div>X</div>
+      </div>
+      <div class="flex flex-col gap-2">
+        <p class="label-text">Choices:</p>
+        <div v-for="choice, index in choices" class="flex items-center gap-1">
+          <button class="btn btn-square btn-sm" :class="{ 'btn-success text-white': choice.correct }"
+            @click="markAsCorrect(choice)">
+            <IconCheck />
+          </button>
+          <textarea :value="choice.text" @input="choice.text = ($event.target as HTMLInputElement).value"
+            :class="{ 'textarea-success': choice.correct }"
+            class="textarea textarea-sm textarea-bordered font-mono flex-1" rows="1">
+          </textarea>
+          <button @click="deleteChoice(index)" :class="{ 'btn-disabled': choices.length <= 2 }"
+            class="btn btn-square btn-sm btn-error text-white">
+            <IconClose />
+          </button>
+        </div>
+        <button v-if="choices.length < 4" @click="choices.push({ text: '', correct: false })"
+          class="btn btn-sm btn-neutral btn-ghost">
+          <IconPlus />Add choice
+        </button>
+      </div>
+      <div>
+        <div class="form-control">
+          <label class="label cursor-pointer">
+            <span class="label-text">Include explanation</span>
+            <input type="checkbox" class="checkbox checkbox-sm" v-model="includeExplanation" />
+          </label>
+        </div>
+        <div v-if="includeExplanation" class="form-control">
+          <label class="label" for="question_explanation">
+            <span class="label-text">Explanation:</span>
+          </label>
+          <textarea v-model="explanation" id="question_explanation"
+            class="textarea textarea-bordered textarea-sm font-mono" rows="5">
+        </textarea>
+        </div>
       </div>
     </div>
+    <div class="divider"></div>
     <h1 class="text-2xl text-main font-semibold text-center font-mono">PREVIEW</h1>
   </main>
 </template>
