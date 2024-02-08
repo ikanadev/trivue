8a3b8a09c17059aa631206c4a4413a533c8c0e6f
Kevin Vargas <vargaskevv@gmail.com>
2024-02-08 17:38:37 -0400

    Questions page

--- src/views/questions/Questions.vue
+++ src/views/questions/Questions.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ThemeButton from "@/components/ThemeButton.vue";
-import { QuizSort, QuizOrder, QuestionLevel, ALL_LEVELS } from "@/utils";
+import { QuizSort, QuizOrder, QuestionLevel, ALL_LEVELS, marked } from "@/utils";
 import { IconSortAscending, IconSortDescending } from "@/components/icons";
 import Pagination from "./Pagination.vue";
+import Star from "./Star.vue";
 
+import DOMPurify from "dompurify";
 import { getQuestions } from "@/api";
 import { ref } from "vue";
 import { useQuery } from "@tanstack/vue-query";
@@ -17,7 +19,7 @@ const { isLoading, isError, data } = useQuery({
 	queryKey: [
 		"questions",
 		{
-			page: 1,
+			page: page,
 			quizLevel: quizLevel,
 			sortBy: sortBy,
 			sortOrder: sortOrder,
@@ -42,10 +44,7 @@ function setPage(n: number) {
 
 <template>
 	<main class="py-2">
-		<div class="flex justify-end">
-			<ThemeButton />
-		</div>
-		<div class="flex justify-end items-end gap-2">
+		<div class="flex items-end gap-2">
 			<label class="form-control">
 				<div class="label pb-0">
 					<span class="label-text">Level</span>
@@ -78,17 +77,36 @@ function setPage(n: number) {
 					<IconSortDescending class="text-lg" />
 				</div>
 			</label>
+			<div class="flex-1"></div>
+
+			<ThemeButton />
 		</div>
 		<div>
 			<div v-if="isLoading" class="flex justify-center h-36">
 				<span class="loading loading-ring loading-lg"></span>
 			</div>
 			<div v-else-if="isError" role="alert" class="alert alert-error">
-				<span>Can't retrieve questions at this time</span>
+				<span class="text-error">Can't retrieve questions at this time</span>
 			</div>
-			<div v-else-if="data">
-				<div>Total items: {{ data.total }}</div>
-				<Pagination :current-page="page" :total-items="32" :set-page="setPage" />
+			<div v-else-if="data" class="mt-4">
+				<div class="flex flex-col gap-4 mb-6">
+					<div v-for="question in data.questions" class="card shadow-lg bg-base-200 card-compact" :key="question.id">
+						<div class="card-body">
+							<div class="card-actions justify-end items-center">
+								<div class="badge">{{ question.level }}</div>
+							</div>
+							<div v-html="DOMPurify.sanitize(marked.parse(question.text) as string)" class="prose"></div>
+							<div class="card-actions justify-between items-center mt-3">
+								<div class="text-sm italic">{{ new Date(question.createdAt).toLocaleString() }}</div>
+								<div class="flex items-center gap-1">
+									<Star width="24" height="24" :points="question.votes.positive - question.votes.negative" />
+									<p class="text-lg">{{ question.votes.positive - question.votes.negative }}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<Pagination :current-page="page" :total-items="data.total" :set-page="setPage" />
 			</div>
 		</div>
 	</main>
