50f76bcf5fee00a47d83a949e53436156df837a4
Kevin Vargas <vargaskevv@gmail.com>
2024-02-07 22:24:42 -0400

    add icons

---
+++ src/components/icons/IconSortDescending.vue
@@ -0,0 +1,13 @@
+
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+		<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+			d="M4 17h12M4 12h9M4 7h6m8 6V5m0 0l3 3m-3-3l-3 3"></path>
+	</svg>
+</template>
+
+<script lang="ts">
+export default {
+	name: 'CiSortDescending'
+}
+</script>
