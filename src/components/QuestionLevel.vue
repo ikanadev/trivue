e220cbddc808f8147d15908099f5624fe6af98d3
Kevin Vargas <vargaskevv@gmail.com>
2024-02-12 17:13:40 -0400

    Question level component

---
+++ src/components/QuestionLevel.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { QuestionLevel } from "@/utils";
+defineProps<{ level: QuestionLevel }>();
+</script>
+<template>
+	<div class="badge badge-outline">{{ level }}</div>
+</template>
