dd1d1f3fb4014251af89b8af7e693b64c75414ec
Kevin Vargas <vargaskevv@gmail.com>
2024-02-12 17:13:48 -0400

    Question Footer component

---
+++ src/components/QuestionFooter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { IconHeart, IconHeartBroken } from "@/components/icons";
+defineProps<{
+	createdAt: string;
+	points: number;
+}>();
+
+</script>
+<template>
+	<div class="flex justify-between items-center">
+		<div class="text-xs italic">{{ new Date(createdAt).toLocaleString() }}</div>
+		<div class="flex items-center gap-1">
+			<IconHeart v-if="points > 0" class="text-xl text-red-500" />
+			<IconHeart v-else-if="points === 0" class="text-xl text-gray-500" />
+			<IconHeartBroken v-else class="text-xl text-gray-500" />
+			<p class="text-lg">{{ points }}</p>
+		</div>
+	</div>
+</template>
